86fa4c38c2b00d5afc9281c1828f325308d42039
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-01-16 12:45:47 +0300

    Implemented GET request functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,29 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=009335a1-08c6-45cb-9cd9-9e6280600a9a`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceURL: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+showRecipe();
