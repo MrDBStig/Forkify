a9551ebbaa95c255b49ff4c6539c9ca9db3fe2f0
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-01-18 15:07:58 +0300

    Implemented pagination functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 // Model
 import * as model from './model.js';
@@ -42,14 +43,26 @@ const controlSearchResults = async function () {
     // Render search results
     // resultsView.render(model.state.search.results);
     resultsView.render(model.getSearchResultPage());
+
+    // Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     // console.error(err);
     recipeView.renderError();
   }
 };
 
+const controlPagination = function (goToPage) {
+  // Render search results
+  resultsView.render(model.getSearchResultPage(goToPage));
+
+  // Render pagination buttons
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
